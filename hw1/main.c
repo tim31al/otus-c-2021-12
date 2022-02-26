1caa206c4903e09618c918ab3542d0b8425b0695
Alexandr Timofeev <tim31al@gmail.com>
2022-02-26 07:13:02 +0300

    fix

--- hw1/main.c
+++ hw1/main.c
@@ -10,7 +10,7 @@
 
 static bool findEndOfCentralDirectory(FILE *, EndOfCentralDirectoryRecord *);
 static bool findCentralDirectoryOffseth(FILE *, EndOfCentralDirectoryRecord *);
-static int exitApp(FILE *, char *);
+static int exitApp(FILE *, EndOfCentralDirectoryRecord*, char *);
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -20,8 +20,8 @@ int main(int argc, char *argv[]) {
 
   FILE *fp;
 
-  CentralDirectoryFileHeader cd_header;
-  EndOfCentralDirectoryRecord eocd;
+  CentralDirectoryFileHeader* cd_header;
+  EndOfCentralDirectoryRecord* eocd;
   size_t cd_size;
 
   char *filename = NULL;
@@ -33,56 +33,66 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  // выделить память, заполнить 0
+  eocd = calloc(1, sizeof(EndOfCentralDirectoryRecord));
+
+
   // Найти EndOfCentralDirectoryRecord
-  if (!findEndOfCentralDirectory(fp, &eocd)) {
-    return exitApp(fp, MESSAGE_IS_NOT_ZIP);
+  if (!findEndOfCentralDirectory(fp, eocd)) {
+    return exitApp(fp, eocd, MESSAGE_IS_NOT_ZIP);
   }
 
   // найти начало архива
-  if (!findCentralDirectoryOffseth(fp, &eocd)) {
-    return exitApp(fp, MESSAGE_ARCH_EMPTY);
+  if (!findCentralDirectoryOffseth(fp, eocd)) {
+    return exitApp(fp, eocd, MESSAGE_IS_NOT_ZIP);
   }
 
   printf("%s\n", MESSAGE_IS_ZIP);
 
+  cd_header = calloc(1, sizeof(CentralDirectoryFileHeader));
   filename = malloc(FILENAME_SIZE);
   cd_size = sizeof(CentralDirectoryFileHeader);
 
   while (!(feof(fp))) {
-    fread(&cd_header, cd_size, 1, fp);
+    fread(cd_header, cd_size, 1, fp);
 
-    if (cd_header.signature == CENTRAL_SIGNATURE) {
-      if (cd_header.filenameLength > FILENAME_SIZE) {
-        filename = malloc(cd_header.filenameLength + 1);
+    if (cd_header->signature == CENTRAL_SIGNATURE) {
+      if (cd_header->filenameLength > FILENAME_SIZE) {
+        filename = malloc(cd_header->filenameLength + 1);
       }
 
-      fread(filename, cd_header.filenameLength, 1, fp);
-      filename[cd_header.filenameLength] = '\0';
+      fread(filename, cd_header->filenameLength, 1, fp);
+      filename[cd_header->filenameLength] = '\0';
       printf("\t%s\n", filename);
-      
+
       counter++;
 
-      fseek(fp, cd_header.fileCommentLength + cd_header.extraFieldLength,
+      fseek(fp, cd_header->fileCommentLength + cd_header->extraFieldLength,
             SEEK_CUR);
     }
   }
 
-  if (counter != eocd.totalCentralDirectoryRecord) {
+  if (counter != eocd->totalCentralDirectoryRecord) {
     printf("Ошибка подсчета файлов.\nВсего: %d\nНайдено: %d\n",
-           eocd.totalCentralDirectoryRecord, counter);
+           eocd->totalCentralDirectoryRecord, counter);
   } else {
-    printf("Всего файлов: %d\n", eocd.totalCentralDirectoryRecord);
+    printf("Всего файлов: %d\n", eocd->totalCentralDirectoryRecord);
   }
 
   fclose(fp);
+
   free(filename);
+  free(eocd);
+  free(cd_header);
 
   return 0;
 }
 
 // Выход из программы
-int exitApp(FILE *fp, char *message) {
+int exitApp(FILE *fp, EndOfCentralDirectoryRecord *eocd, char *message) {
   fclose(fp);
+  free(eocd);
+
   printf("%s\n", message);
   return 0;
 }
@@ -103,7 +113,7 @@ bool findEndOfCentralDirectory(FILE *fp, EndOfCentralDirectoryRecord *eocd) {
     fread(&signature, 4, 1, fp);
 
     if (signature == END_SIGNATURE) {
-      fread(eocd, sizeof(eocd), 1, fp);
+      fread(eocd, sizeof(EndOfCentralDirectoryRecord), 1, fp);
       is_find = true;
 
       break;
@@ -121,13 +131,19 @@ bool findEndOfCentralDirectory(FILE *fp, EndOfCentralDirectoryRecord *eocd) {
 bool findCentralDirectoryOffseth(FILE *fp, EndOfCentralDirectoryRecord *eocd) {
   uint32_t signature;
   bool is_find = false;
+  int read_bytes = 0;
 
   // переместить указатель на возможное (не учтена картинка) начало записей
   // Central Directory
   fseek(fp, eocd->centralDirectoryOffset, SEEK_SET);
 
+
   while (!(feof(fp))) {
-    fread(&signature, 4, 1, fp);
+    read_bytes = fread(&signature, 4, 1, fp);
+    if (read_bytes == 0) {
+      break;
+    }
+
     if (signature == CENTRAL_SIGNATURE) {
       fseek(fp, -4, SEEK_CUR);
       is_find = true;
